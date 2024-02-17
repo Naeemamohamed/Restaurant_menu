aee6a8eb10b0583e91de1b6dfc9a537665c2655d
Naeemamohamed <mohamednaima2006@gmail.com>
2024-02-17 13:25:03 +0300

    add concat

--- js_projects/js01/Restaurant_menu/Restaurant_menu/app.js
+++ js_projects/js01/Restaurant_menu/Restaurant_menu/app.js
@@ -3,6 +3,9 @@ let num1 = 3;
 let num2 = 2;
 let shopping = [ 'bread', 'sugar', 'milk']
 let solution = num1 * num2;
+let name = "Naeema";
 
+let newMessage = ` ${message} my name is ${name}` 
 console.log(solution); 
 console.log(shopping)
+console.log(newMessage);
